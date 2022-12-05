4041bed0a8ff64081ce108eee048534be027bf65
CRMclaughlin <chrisraymusik@gmail.com>
2022-12-05 14:24:48 -0600

    added require and export to controllers

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const sequalize = require('../config/connection');
+
+
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const viewRoutes = require('./viewRoutes');
+const dashboardRoutes = require('./dashboard-routes')
 
 router.use('/', viewRoutes);
+router.use('/dashboard', dashboardRoutes)
 router.use('/api', apiRoutes)
 
 module.exports = router;
